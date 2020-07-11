52edb2d63e09db25607bde825e6b5c1629c142f2
shaunkleyn <kleyn.shaun@gmail.com>
2020-07-11 11:23:25 +0200

    added titleCase pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FavoriteComponent } from './favorite/favorite.component';
+import { HelloComponent } from './hello.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FavoriteComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, TitleCasePipe ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
