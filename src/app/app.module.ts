95389aa2bd4ac6bd6bb8cbc1164171bcb1dc7d7c
shaunkleyn <kleyn.shaun@gmail.com>
2020-07-11 14:15:03 +0200

    1 file modified, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, FavoriteComponent, PanelComponent, LikeComponent ],
+  declarations: [ AppComponent, LikeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
