9fd366e2ff996a8b21385a5dc3aedc363b5b35da
shaunkleyn <kleyn.shaun@gmail.com>
2020-07-11 09:43:40 +0200

    added authors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, FavoriteComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, FavoriteComponent, AuthorsComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [AuthorsService]
 })
 export class AppModule { }
