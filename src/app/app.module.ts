33eca62a6b5db9e3c79fe9e47cac82ea134b8591
shaunkleyn <kleyn.shaun@gmail.com>
2020-07-11 09:44:29 +0200

    removed favorite component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FavoriteComponent } from './favorite/favorite.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, FavoriteComponent, AuthorsComponent ],
+  declarations: [ AppComponent, AuthorsComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AuthorsService]
 })
