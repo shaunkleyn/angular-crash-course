a53905dbde6b524d4464bbfa19edca557f35597a
shaunkleyn <kleyn.shaun@gmail.com>
2020-07-12 11:54:19 +0200

    1 file modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ZippyComponent } from './zippy/zippy.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ZippyComponent, ContactDetailsComponent ],
+  declarations: [ AppComponent, ContactDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
